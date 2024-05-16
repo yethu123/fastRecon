df248c50a8aed45521347c51f4d4510dfea4d587
yethu123 <thudodoy@gmail.com>
2024-05-16 11:34:04 +0630

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+# Install tools via go
+go install -v github.com/tomnomnom/assetfinder@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/tomnomnom/gf@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+go install github.com/hahwul/dalfox/v2@latest
+go install github.com/projectdiscovery/notify/cmd/notify@latest
+
+# Create a tools folder
+mkdir -p ~/tools
+cd ~/tools/
+
+# Install system packages
+apt-get update
+apt-get install -y \
+  libcurl4-openssl-dev \
+  libssl-dev \
+  jq \
+  ruby-full \
+  libxml2 \
+  libxml2-dev \
+  libxslt1-dev \
+  ruby-dev \
+  build-essential \
+  libgmp-dev \
+  zlib1g-dev \
+  libffi-dev \
+  python-dev \
+  python-setuptools \
+  libldns-dev \
+  python3-pip \
+  python-pip \
+  python-dnspython \
+  git \
+  rename \
+  xargs \
+  wafw00f \
+  dirsearch \
+  gobuster \
+  feroxbuster \
+  awscli \
+  nmap
+
+# Reminder for AWS credentials
+echo "Don't forget to set up AWS credentials!"
+
+# Install Chromium
+echo "Installing Chromium"
+sudo snap install chromium
+echo "done"
+
+# Clone and install various tools
+echo "installing JSParser"
+git clone https://github.com/nahamsec/JSParser.git
+cd JSParser
+sudo python setup.py install
+cd ~/tools/
+echo "done"
+
+echo "installing Sublist3r"
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r
+pip install -r requirements.txt
+cd ~/tools/
+echo "done"
+
+echo "installing teh_s3_bucketeers"
+git clone https://github.com/tomdev/teh_s3_bucketeers.git
+cd ~/tools/
+echo "done"
+
+echo "installing wpscan"
+git clone https://github.com/wpscanteam/wpscan.git
+cd wpscan
+sudo gem install bundler && bundle install --without test
+cd ~/tools/
+echo "done"
+
+echo "installing dirsearch"
+git clone https://github.com/maurosoria/dirsearch.git
+cd ~/tools/
+echo "done"
+
+echo "installing lazys3"
+git clone https://github.com/nahamsec/lazys3.git
+cd ~/tools/
+echo "done"
+
+echo "installing virtual host discovery"
+git clone https://github.com/jobertabma/virtual-host-discovery.git
+cd ~/tools/
+echo "done"
+
+echo "installing sqlmap"
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+cd ~/tools/
+echo "done"
+
+echo "installing knock.py"
+git clone https://github.com/guelfoweb/knock.git
+cd ~/tools/
+echo "done"
+
+echo "installing lazyrecon"
+git clone https://github.com/nahamsec/lazyrecon.git
+cd ~/tools/
+echo "done"
+
+echo "installing massdns"
+git clone https://github.com/blechschmidt/massdns.git
+cd massdns
+make
+cd ~/tools/
+echo "done"
+
+echo "installing asnlookup"
+git clone https://github.com/yassineaboukir/asnlookup.git
+cd asnlookup
+pip install -r requirements.txt
+cd ~/tools/
+echo "done"
+
+echo "installing httprobe"
+go get -u github.com/tomnomnom/httprobe 
+echo "done"
+
+echo "installing unfurl"
+go get -u github.com/tomnomnom/unfurl 
+echo "done"
+
+echo "installing crtndstry"
+git clone https://github.com/nahamsec/crtndstry.git
+echo "done"
+
+echo "downloading Seclists"
+cd ~/tools/
+git clone https://github.com/danielmiessler/SecLists.git
+cd ~/tools/SecLists/Discovery/DNS/
+cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
+cd ~/tools/
+echo "done"
+
+echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
+ls -la
+echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
+
+
